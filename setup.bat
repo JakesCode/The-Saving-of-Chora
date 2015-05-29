a15e94d152bc9452786a0907dd86d774c203bf04
JakesCode <stringcraftgaming@gmail.com>
2015-05-29 10:45:44 +0100

    Added an install file, "setup.bat"

---
+++ setup.bat
@@ -0,0 +1,31 @@
+cd C:\Python34\The-Saving-of-Chora-master
+
+cd Install
+
+echo Now installing libraries:
+
+cd clint-master
+
+python setup.py install
+
+cd Install
+
+cd colorama-0.3.3
+
+python setup.py install
+
+cd Install
+
+cd setuptools-16.0
+
+python setup.py install
+
+cd Install
+
+cd termcolor-1.1.0
+
+python setup.py install
+
+echo "All done."
+
+pause
